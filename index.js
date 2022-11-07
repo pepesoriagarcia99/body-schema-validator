d716b3b9979f1e6ef695b5276fad0dbe705440c8
Soria Garcia, Jose Eduardo <jesoria@indra.es>
2022-11-07 16:41:31 +0100

    configuracion Eslint y comandos npm

---
+++ index.js
@@ -0,0 +1,24 @@
+import Validator from 'src/model/Validator';
+
+/**
+ * Middleware
+ *
+ * @param {Object} schema schema element
+ * @returns {Function} Express callback
+ */
+export const body =
+  (schema = {}) =>
+    (req, res, next) => {
+      const { valid, message, data } = new Validator(schema, req.body);
+
+      if (valid) {
+        req.body = data;
+        next();
+      }
+      else {
+        res.status(400).json(message)
+          .end();
+      }
+    };
+
+export { default as Validator } from '@/model/Validator';
